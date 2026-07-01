9118ddef603261b626b1af6d06c7db6685b7cfad
Amir mohammad <amir.talaee1386@gmail.com>
2026-07-01 16:37:28 +0330

    wip: update main app startup entry point

--- src/main/java/Client/MainApp.java
+++ src/main/java/Client/MainApp.java
@@ -1,4 +1,32 @@
 package Client;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Client/Login.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 850, 650);
+
+
+            primaryStage.setTitle("X - Login");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] eloquence) {
+        launch(eloquence);
+    }
 }
