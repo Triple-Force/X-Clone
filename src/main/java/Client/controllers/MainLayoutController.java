f023b361fb37cba08e9631ac9dcd6a2414bdd7f4
Amir mohammad <amir.talaee1386@gmail.com>
2026-07-16 14:11:54 +0330

    wip(controller): start implementing MainLayoutController logic

---
+++ src/main/java/Client/controllers/MainLayoutController.java
@@ -0,0 +1,12 @@
+package Client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
+
+public class MainLayoutController {
+
+}
