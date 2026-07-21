020d45824d051f6db21515d66f81d692fcef9aed
mohammadreza <mohammadrezaashrafian2@gmail.com>
2026-07-21 16:14:24 +0330

    config(client): disable connection timeout by setting timeoutMs to 0

--- src/main/java/Client/config/ServerConfig.java
+++ src/main/java/Client/config/ServerConfig.java
@@ -4,6 +4,6 @@ public record ServerConfig(String host, int port, int timeoutMs)
 {
     public static ServerConfig defaultLocal()
     {
-        return new ServerConfig("127.0.0.1", 8080, 5000);
+        return new ServerConfig("127.0.0.1", 8080, 0);
     }
 }
